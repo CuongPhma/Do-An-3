661358f2feb0ccc7b225aeb747ff3d47f847b190
Pham Chi Cuong <cuongfcban@gmail.com>
2025-05-26 21:50:59 +0700

    test commit file src, format lai code cho gon/ viet het thanh ham

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -3,17 +3,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <MillisScheduler.h>
-
 #include <Arduino.h> // Thư viện này thường được bao gồm tự động, nhưng nếu gặp lỗi có thể thêm vào
-
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 MillisScheduler scheduler;
 
-void sayHi() {
-  Serial.println("👋 Hi mỗi 1 giây");
-}
-void sayHi1() {
-  Serial.println("👋 Hi mỗi 4 giây");
-}
+// ==== DHT Config ====
+#define DHTPIN 5 // Chân kết nối DHT
+#define DHTTYPE    DHT11     // DHT 11
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
 // ==== LED Config ====
 #define RED_PIN    32
 #define GREEN_PIN  25
@@ -21,6 +22,7 @@ void sayHi1() {
 int red = 250;
 int green = 250;
 int blue = 250;
+
 // ==== RFID Config ====
 #define SS_PIN 21   // SDA
 #define RST_PIN 22  // RST
@@ -37,54 +39,37 @@ MFRC522 rfid(SS_PIN, RST_PIN);  // Tạo đối tượng RFID
 #define USER_EMAIL "admin@gmail.com"
 #define USER_PASSWORD "admin123"
 
+// ==== Firebase Instances ====
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
 void streamCallback(FirebaseStream data);
 void streamTimeoutCallback(bool timeout);
 void dump_byte_array(byte *buffer, byte bufferSize);
 void setColor(int r, int g, int b);
 void changeColor(int r, int g, int b);
 void randomColor();
-
-// ==== Firebase Instances ====
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
+void RC522();
+void DHT_11();
+void SetUp_DHT11();
+void SetUp_MFRC522();
+void SetUp_FireBase();
+void SetUp_WiFi();
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("🔌 Đang kết nối WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("\n✅ WiFi đã kết nối!");
+  SetUp_WiFi(); // Kết nối WiFi
+  SetUp_FireBase(); // Khởi tạo Firebase
+  SetUp_MFRC522(); // Khởi tạo RFID
+  SetUp_DHT11(); // Khởi tạo cảm biến DHT11
+
   pinMode(2, OUTPUT);
 
-  // scheduler.addTask(1000, sayHi);   // 1s nói hi
-  // scheduler.addTask(4000, sayHi1);  // 4s nói hi
-  /////////////////////////////////////////////////////////////////////////////
-  // Cấu hình Firebase
-  /////////////////////////////////////////////////////////////////////////////
-  config.api_key = API_KEY;
-  config.database_url = DATABASE_URL;
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
-  // Bắt đầu lắng nghe thay đổi ở node /devices/den/status
-  if (!Firebase.RTDB.beginStream(&fbdo, "/devices/den/status")) {
-    Serial.println(fbdo.errorReason());
-  }
-  // Gán callback khi có thay đổi từ Firebase
-  Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
-
-  /////////////////////////////////////////////////////////////////////////////
-  // Khởi tạo các reader MFRC522
-  /////////////////////////////////////////////////////////////////////////////
-  SPI.begin();       // Khởi động SPI
-  rfid.PCD_Init();   // Khởi động module RC522
-
-  Serial.println("👉 Quét thẻ RFID...");
+  //scheduler.addTask(200, RC522);   // 200ms quét thẻ RFID
+  scheduler.addTask(1000, RC522); // 1000ms quét thẻ RFID
+  scheduler.addTask(1000, DHT_11); // 1000ms đọc cảm biến DHT11
+  
 }
 
 // ==== Callback khi Firebase có thay đổi ====
@@ -112,8 +97,8 @@ void streamTimeoutCallback(bool timeout) {
 
 void loop() {
     
-    scheduler.update(); // phải gọi trong loop
-
+  scheduler.update(); // phải gọi trong loop
+ 
 }
 
 // Hàm đổi màu (Common Anode thì dùng 255 - value)
@@ -137,6 +122,41 @@ void setColor(int r, int g, int b) {
   analogWrite(BLUE_PIN, b);
 }
 
+void SetUp_WiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("🔌 Đang kết nối WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\n✅ WiFi đã kết nối!");
+}
+
+void SetUp_FireBase()
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Cấu hình Firebase
+  /////////////////////////////////////////////////////////////////////////////
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+  // Bắt đầu lắng nghe thay đổi ở node /devices/den/status
+  if (!Firebase.RTDB.beginStream(&fbdo, "/devices/den/status")) {
+    Serial.println(fbdo.errorReason());
+  }
+  // Gán callback khi có thay đổi từ Firebase
+  Firebase.RTDB.setStreamCallback(&fbdo, streamCallback, streamTimeoutCallback);
+}
+
+void SetUp_MFRC522() {
+  // Khởi tạo các reader MFRC522
+  SPI.begin();       // Khởi động SPI
+  rfid.PCD_Init();   // Khởi động module RC522
+  Serial.println("👉 Quét thẻ RFID...");
+}
 void RC522(){
  // Kiểm tra có thẻ mới không
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
@@ -157,11 +177,50 @@ void RC522(){
   rfid.PCD_StopCrypto1();
 }
 
-
+// Hàm này sẽ được gọi định kỳ để thực hiện các tác vụ theo khoảng thời gian nhất định
+// interval: khoảng thời gian giữa các lần thực hiện (tính bằng ms)
 void Task(unsigned long interval, void (*func)()) {
   static unsigned long lastRun = 0;
   if (millis() - lastRun >= interval) {
     lastRun = millis();  // Cập nhật thời điểm thực hiện
     func();              // Gọi hàm truyền vào
+  }
+}
+
+
+void SetUp_DHT11()
+{
+  // Initialize device.
+  dht.begin();
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+void DHT_11()
+{
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
   }
 }
